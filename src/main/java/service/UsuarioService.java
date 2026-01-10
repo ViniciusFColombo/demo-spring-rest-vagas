aa958eccca814f28f44191d56df8fa0f3fcfa2c2
Vinicius Colombo <colombov54@gmail.com>
2026-01-10 18:32:29 -0300

    Metodo Post - criar novo usuário

--- src/main/java/service/UsuarioService.java
+++ src/main/java/service/UsuarioService.java
@@ -1,5 +1,7 @@
 package service;
 
+import entities.Usuario;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -9,4 +11,9 @@ import repository.UsuarioRepository;
 @Service
 public class UsuarioService {
 	private final UsuarioRepository usuarioRepository;
+
+	@Transactional
+	public Usuario salvar (Usuario usuario){
+		return usuarioRepository.save(usuario);
+	}
 }
