5827d562796df0001096a410abfe988c9af802f0
Vinicius Colombo <colombov54@gmail.com>
2026-01-14 17:35:05 -0300

    Confidurando SpringDoc OpenApi 3.1 e Swagger

---
+++ src/main/java/com/projetoscurso/demo_parki_api/config/SpringDocOpenApiConfig.java
@@ -0,0 +1,23 @@
+package com.projetoscurso.demo_parki_api.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocOpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().info(new Info()
+                .title("REST API - Spring Paark")
+                .description("API para getsão de estacionamento de veiculos")
+                .version("v1")
+                .license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0.html"))
+                .contact(new Contact().name("Vinicius Colombo").email("vsf765@gmail.com"))
+        );
+    }
+}
