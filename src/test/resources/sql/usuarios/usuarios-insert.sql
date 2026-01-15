6413bf6c3709c3d6250008502eef5cda7e36b6c3
Vinicius Colombo <colombov54@gmail.com>
2026-01-15 17:48:03 -0300

    Configurando ambiente de testes

---
+++ src/test/resources/sql/usuarios/usuarios-insert.sql
@@ -0,0 +1,3 @@
+insert into USUARIOS (id, username, password, role) values (100, 'ana@email.com', '123456', 'ROLE_ADMIN');
+insert into USUARIOS (id, username, password, role) values (100, 'bia@email.com', '123456', 'ROLE_CLIENTE');
+insert into USUARIOS (id, username, password, role) values (100, 'bob@email.com', '123456', 'ROLE_CLIENTE');
